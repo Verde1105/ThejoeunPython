5a55d163ec5690267d3d2315e951a9baacd4c163
Verde1105 <lunaflo@naver.com>
2023-05-31 00:35:34 +0900

    첫번째 파이썬

---
+++ STUDY/test.py
@@ -0,0 +1,7 @@
+# 실행 명령어
+# python test.py
+# 상위 디렉토리로 이동 : cd ..
+
+print("test")
+lang='apple';
+print(lang[:]);
---
+++ STUDY/test2.py
@@ -0,0 +1,5 @@
+# 실행 명령어
+# python test.py
+# 상위 디렉토리로 이동 : cd ..
+
+print("test2")
